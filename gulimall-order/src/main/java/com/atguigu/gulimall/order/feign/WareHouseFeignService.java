a87ceefe5c5da235a9ef405e5d2deccc19b098cf
Userzrq <userzrq@126.com>
2020-11-04 10:26:36 +0800

    库存feign远程服务

---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/feign/WareHouseFeignService.java
@@ -0,0 +1,26 @@
+package com.atguigu.gulimall.order.feign;
+
+import com.atguigu.gulimall.commons.bean.Resp;
+import com.atguigu.gulimall.order.vo.ware.LockStockVo;
+import com.atguigu.gulimall.order.vo.ware.SkuLockVo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ * @author 10017
+ */
+@FeignClient("gulimall-wms")
+public interface WareHouseFeignService {
+
+    /**
+     * 验证库存并锁库存
+     *
+     * @param skuLockVos
+     * @return
+     */
+    @GetMapping("wms/waresku/checkAndLock")
+    public Resp<LockStockVo> lockAndCheckStock(@RequestBody List<SkuLockVo> skuLockVos);
+}
